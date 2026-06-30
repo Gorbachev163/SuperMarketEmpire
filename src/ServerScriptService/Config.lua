1bd46baa42d45e888a17af36a8b647372fada924
Gorbachev163 <gorbachev_163@mail.ru>
2026-06-30 16:12:39 +0400

    Add Config.lua for game settings and economy

---
+++ src/ServerScriptService/Config.lua
@@ -0,0 +1,17 @@
+local Config = {}
+
+-- Деньги
+Config.StartMoney = 5000
+
+-- Магазин
+Config.StoreName = "SuperMarket Empire"
+
+-- Покупатели
+Config.CustomerSpawnTime = 5
+Config.MaxCustomers = 15
+
+-- Экономика
+Config.DefaultProductPrice = 10
+Config.DefaultProductCost = 5
+
+return Config
