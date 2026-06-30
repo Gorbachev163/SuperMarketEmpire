c8054d5e02508209a57e577e2b3f623a0a3d8cea
Gorbachev163 <gorbachev_163@mail.ru>
2026-06-30 16:03:36 +0400

    Add initial configuration settings for the game

---
+++ src/ReplicatedStorage/Config.lua
@@ -0,0 +1,23 @@
+local Config = {}
+
+-- Игрок
+Config.StartMoney = 5000
+
+-- Магазин
+Config.StoreName = "SuperMarket Empire"
+
+-- Покупатели
+Config.CustomerSpawnTime = 5
+Config.MaxCustomers = 15
+
+-- Экономика
+Config.DefaultProductPrice = 10
+Config.DefaultProductCost = 5
+
+-- Полки
+Config.ShelfCapacity = 20
+
+-- Кассы
+Config.CheckoutTime = 3
+
+return Config
